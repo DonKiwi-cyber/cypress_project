004882a7c2bba93f1812e0d1f334b8fe7dcb63f8
DonKiwi-cyber <106345550+DonKiwi-cyber@users.noreply.github.com>
2023-12-05 10:10:19 -0600

    login visual updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { UiComponent } from './ui/ui.component';
-import { AppComponent } from './app.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
